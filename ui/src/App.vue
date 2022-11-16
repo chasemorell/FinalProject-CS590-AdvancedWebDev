5ddf6b715001dedee7bae9cb816af49775e12afe
Chase Morell <chase.morell@duke.edu>
2022-11-16 18:42:42 -0500

    theme changes

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,14 +1,15 @@
 <template>
   <div>
-    <b-navbar toggleable="lg" type="dark" :variant="user?.roles?.includes('operator') ? 'info' : 'primary'">
+    <b-navbar toggleable="lg" type="dark" :variant="'dark'">
       <b-navbar-brand href="#">
-        <span v-if="user?.name">Welcome, {{ user.name }}</span>
-        <span v-else>Lastnight</span>
+        <img src="@/assets/logo.png" alt="Logo" class="d-inline-block align-top" style="max-height:8vh" >
       </b-navbar-brand>
       <b-navbar-nav>
-        <b-nav-item href="/">All Orders</b-nav-item>
-        <b-nav-item v-if="user?.roles?.includes('customer')" href="/customer">My Orders</b-nav-item>
-        <b-nav-item v-if="user?.roles?.includes('operator')" href="/operator">My Work Screen</b-nav-item>
+        <b-nav-item href="/">Home</b-nav-item>
+        <b-nav-item v-if="user?.roles?.includes('customer')" href="/customer">My Groups</b-nav-item>
+        <b-nav-item v-if="user?.roles?.includes('customer')" href="/customer">Search</b-nav-item>
+        <b-nav-item v-if="user?.roles?.includes('operator')" href="/operator">(Old) My Work Screen</b-nav-item>
+
         <b-nav-item v-if="user?.name == null" href="/api/login">Login</b-nav-item>
         <b-nav-item v-if="user?.name" @click="logout">Logout</b-nav-item>
         <form method="POST" action="/api/logout" id="logoutForm" />
@@ -32,3 +33,9 @@ function logout() {
   ;(window.document.getElementById('logoutForm') as HTMLFormElement).submit()  
 }
 </script>
+
+<style scoped>
+ .navbar.navbar-dark.bg-dark{
+    background-color: #363636!important;
+ }
+</style>
